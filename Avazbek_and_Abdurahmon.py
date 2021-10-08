086d6ab13bf51b92ec6947652bc3a4ab08af0a1f
Abdurahmon <you@examole.com>
2021-10-08 17:31:10 +0500

    login

--- Avazbek_and_Abdurahmon.py
+++ Avazbek_and_Abdurahmon.py
@@ -1,73 +1,5 @@
-ents[3]:
-            self.log_out()
-        elif user_wants == elements[4]:
-            self.del_accunt()     
-    def register (self):
-        self.cls()
-        name = input("Enter name: ").strip().lower()
-        while not name.isalpha() or len(name) < 3:
-            self.cls()
-            print(f"To'g'ri kiriting...")
-            name = input("Enter name: ").strip().lower()
-            
-        
-            
-        self.cls()
-        surname = input("Enter surname: ").strip().lower()
-        while not surname.isalpha() or len(surname) < 3:
-            self.cls()
-            surname = input("Enter surname: ").strip().lower()   
-        
-        
-        
-        self.cls()
-        age = input ("Enter age: ").strip()
-        while not age.isnumeric():
-            self.cls()
-            age = input ("Enter age: ").strip()
-        
-        
-        self.cls()
-        logn = input ("Enter login: ").strip().lower()
-        while not logn.isalnum():
-            self.cls()
-            logn = input ("Enter login: ").strip().lower()
+import os
 
-
-
-        self.cls()
-        password1 = input ("Enter password: ").strip().lower()
-        while self.str_empty(password1):
-            self.cls()
-            password1 = input ("Enter password: ").strip().lower()
-            
-
-
-        
-
-        
-    def log_in (self):
-        print("login")
-
-
-    def change_log(self):
-        print("changlog")
-
-
-    def log_out (self):
-        print("logout")
-
-
-    def del_accunt(self):
-        print("del")
-    
-    @staticmethod
-    def cls():
-        os.system("clear")
-
-    @staticmethod
-    def str_empty(str_):
-        return not bool(str_)import os
 
 class Users:
     def __init__(self):
@@ -76,8 +8,8 @@ class Users:
         self.age = None
         self.log = None
         self.password = None
-    
-    def tizimga_kirish (self):
+
+    def tizimga_kirish(self):
         self.cls()
         self.dizayner()
         elements = ['1', '2', '3', '4', '5']
@@ -85,7 +17,7 @@ class Users:
         while user_wants not in elements:
             print("To'g'ri kiriting.")
             self.cls()
-            self.dizayner()        
+            self.dizayner()
             user_wants = input(">>> ")
         if user_wants == elements[0]:
             self.register()
@@ -93,26 +25,68 @@ class Users:
             self.log_in()
         elif user_wants == elements[2]:
             self.change_log()
-        elif user_wants == elem
+        elif user_wants == elements[3]:
+            self.log_out()
+        elif user_wants == elements[4]:
+            self.del_accunt()
 
+    def register(self):
+        self.cls()
+        name = input("Enter name: ").strip().lower()
+        while not name.isalpha() or len(name) < 3:
+            self.cls()
+            print(f"To'g'ri kiriting...")
+            name = input("Enter name: ").strip().lower()
 
+        self.cls()
+        surname = input("Enter surname: ")
+        while not surname.isalpha() or len(surname) < 3:
+            self.cls()
+            surname = input("Enter surname: ")
 
+        self.cls()
+        age = input("Enter age: ").strip()
+        while not age.isnumeric():
+            self.cls()
+            age = input("Enter age: ").strip()
 
+        self.cls()
+        logn = input("Enter login: ").strip().lower()
+        while not logn.isalnum():
+            self.cls()
+            logn = input("Enter login: ").strip().lower()
 
+        self.cls()
+        password1 = input("Enter password: ").strip().lower()
+        while self.str_empty(password1):
+            self.cls()
+            password1 = input("Enter password: ").strip().lower()
 
+    def log_in(self):
+        print("login")
 
+    def change_log(self):
+        print("changlog")
 
+    def log_out(self):
+        print("logout")
 
+    def del_accunt(self):
+        print("del")
 
+    @staticmethod
+    def cls():
+        os.system("clear")
 
-
-
+    @staticmethod
+    def str_empty(str_):
+        return bool(str_)
 
     @staticmethod
     def dizayner():
         print("""
   <<<<<<<<<<<< Tizimga_kirish>>>>>>>>>>>> 
-            
+
             Register           [1]
         -------------------------------
             Login              [2]
@@ -122,9 +96,8 @@ class Users:
             Log out            [4]
         -------------------------------
             Delete account     [5]
-            
-        """)
 
+        """)
 
 
 user = Users()
